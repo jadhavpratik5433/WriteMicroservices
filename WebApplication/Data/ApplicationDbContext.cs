e685acaeaaedf68d00aa88dca23d566b1a344b31
jadhavpratik5433 <jadhavpratik5433@gmail.com>
2026-01-10 14:29:05 +0530

    est

---
+++ WebApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication.Entities;
+
+namespace WebApplication.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
--- WebApplication/Program.cs
+++ WebApplication/Program.cs
@@ -1,8 +1,11 @@
+using WebApplication.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
